f8d79d87711ce586d26fe1390e55602e1622ca03
Supasek Sangket <supasek.s@ku.th>
2026-02-18 19:54:02 +0700

    Fix Bug App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
-import TodoItem from './Todoitem.jsx'
+import TodoItem from './TodoItem.jsx'
 
 function App() {
   const TODOLIST_API_URL = 'http://localhost:5000/api/todos/';
