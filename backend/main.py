27370d9697cb123a5efc187c816f4c41f11c49fb
MixSupasek <mixjang04@gmail.com>
2026-04-04 22:55:51 +0700

    Revert "Deploy"
    This reverts commit 9a45e612d908921c2e3891902264396e508092ca.

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 ###from flask_sqlalchemy import SQLAlchemy
 ###from sqlalchemy.orm import DeclarativeBase
@@ -15,15 +15,8 @@ import os
 
 app = Flask(__name__)
 CORS(app)
-
-try:
-    from local_config import CONFIG_DB_URI, CONFIG_JWT_SECRET
-except:
-    CONFIG_DB_URI = 'sqlite:///todos.db'
-    CONFIG_JWT_SECRET = 'fdslkfjsdlkufewhjroiewurewrew'
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI', CONFIG_DB_URI)
-app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', CONFIG_JWT_SECRET)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///todos.db') 
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY','')
 jwt = JWTManager(app)
 
 db.init_app(app) 
@@ -127,17 +120,4 @@ def login():
 
     access_token = create_access_token(identity=user.username)
     return jsonify(access_token=access_token)
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve_frontend(path):
-    static_dir = os.path.join(app.root_path, 'frontend-static')
-    # If a specific file is requested and exists, serve it
-    if path and os.path.isfile(os.path.join(static_dir, path)):
-        return send_from_directory('frontend-static', path)
-    # Otherwise serve the app entrypoint
-    return send_from_directory('frontend-static', 'index.html')
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,8 @@ import TodoList from './TodoList.jsx'
 import PrivateRoute from './PrivateRoute.jsx';
 
 function App() {
-  const TODOLIST_LOGIN_URL = '/api/login/';
-  const TODOLIST_API_URL = '/api/todos/';
+  const TODOLIST_LOGIN_URL = 'http://localhost:5000/api/login/';
+  const TODOLIST_API_URL = 'http://localhost:5000/api/todos/';
 
   return (
     <AuthProvider>
